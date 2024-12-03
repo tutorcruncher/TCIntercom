bfd575fdca788bd358474edd7d4d5dfe0a54d18d
DanJezeph15 <danieljezeph@hotmail.co.uk>
2024-12-03 11:44:28 +0000

    Remove prints

--- tcintercom/app/views.py
+++ tcintercom/app/views.py
@@ -45,10 +45,6 @@ async def validate_ic_webhook_signature(request: Request):
         return
     header_signature = request.headers.get('x-hub-signature', '')
     payload = await request.body()
-    print(header_signature)
-    print(payload)
-    print(app_settings.ic_client_secret)
-    print(f'sha1={hmac.new(app_settings.ic_client_secret.encode(), payload, hashlib.sha1).hexdigest()}')
     assert (
         f'sha1={hmac.new(app_settings.ic_client_secret.encode(), payload, hashlib.sha1).hexdigest()}'
         == header_signature
